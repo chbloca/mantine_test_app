a4964ce47478c9b9a86a037516c63b00fab6876d
Christen <christen.bc@gmail.com>
2023-01-29 11:42:53 +0100

    add custom fontFamily

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import { Checkbox, Group, MantineProvider, Radio } from '@mantine/core';
+import { Button, MantineProvider } from '@mantine/core';
 
 export default function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider theme={{ fontFamily: 'Open Sans' }} withGlobalStyles withNormalizeCSS>
       <Demo />
     </MantineProvider>
   );
 }
 
 function Demo() {
-  return (
-    <>
-      <Group>
-        <Checkbox label="Default checkbox" />
-        <Radio label="Default radio" value="pointer" />
-      </Group>
-
-      <MantineProvider inherit theme={{ cursorType: 'pointer' }}>
-        <Group mt="xl">
-          <Checkbox label="Checkbox with pointer" />
-          <Radio label="Radio with pointer" value="pointer" />
-        </Group>
-      </MantineProvider>
-    </>
-  );
+  return <Button>My app button</Button>;
 }
