d3a3e0e34f1888331f14bd3707157423ffba43a1
Christen <christen.bc@gmail.com>
2023-01-29 12:45:55 +0100

    test AspectRatio

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { AppShell, Aside, Burger, Footer, Header, MediaQuery, Navbar, useMantineTheme, Text } from "@mantine/core";
-import { useState } from "react";
+import { AspectRatio, Image } from "@mantine/core";
 
 export default function App() {
   return (
@@ -8,53 +7,12 @@ export default function App() {
 }
 
 function Demo() {
-  const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
   return (
-    <AppShell
-      styles={{
-        main: {
-          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-        },
-      }}
-      navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"
-      navbar={
-        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
-          <Text>Application navbar</Text>
-        </Navbar>
-      }
-      aside={
-        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-            <Text>Application sidebar</Text>
-          </Aside>
-        </MediaQuery>
-      }
-      footer={
-        <Footer height={60} p="md">
-          Application footer
-        </Footer>
-      }
-      header={
-        <Header height={{ base: 50, md: 70 }} p="md">
-          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
-
-            <Text>Application header</Text>
-          </div>
-        </Header>
-      }
-    >
-      <Text>Resize app to see responsive navbar in action</Text>
-    </AppShell>
+    <AspectRatio ratio={720 / 1080} sx={{ maxWidth: 300 }} mx="auto">
+      <Image
+        src="https://images.unsplash.com/photo-1527118732049-c88155f2107c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
+        alt="Panda"
+      />
+    </AspectRatio>
   );
 }
