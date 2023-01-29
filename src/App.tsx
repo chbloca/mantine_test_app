95317d53a6537f3340ab0cebc81a987268f49ff4
Christen <christen.bc@gmail.com>
2023-01-29 11:25:56 +0100

    replace screen with slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-import { MantineProvider, Text } from '@mantine/core';
+import { MantineProvider, Slider } from '@mantine/core';
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
+      <Demo />
     </MantineProvider>
+  );
+}
+
+const marks = [
+  { value: 20, label: '20%' },
+  { value: 50, label: '50%' },
+  { value: 80, label: '80%' },
+];
+
+function Demo() {
+  return (
+    <Slider
+      defaultValue={40}
+      marks={marks}
+      labelTransition="fade"
+      size={2}
+      styles={(theme) => ({
+        track: {
+          backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.blue[1],
+        },
+        mark: {
+          width: 6,
+          height: 6,
+          borderRadius: 6,
+          transform: 'translateX(-3px) translateY(-2px)',
+          borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.blue[1],
+        },
+        markFilled: {
+          borderColor: theme.colors.blue[6],
+        },
+        markLabel: { fontSize: theme.fontSizes.xs, marginBottom: 5, marginTop: 0 },
+        thumb: {
+          height: 16,
+          width: 16,
+          backgroundColor: theme.white,
+          borderWidth: 1,
+          boxShadow: theme.shadows.sm,
+        },
+      })}
+    />
   );
 }
