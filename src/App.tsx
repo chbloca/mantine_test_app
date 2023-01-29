2dad8735c481d3b4f38565973cd5f85973f5efa8
Christen <christen.bc@gmail.com>
2023-01-29 12:04:50 +0100

    test Default props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Code, MantineProvider, Title } from '@mantine/core';
+import { Button, Group, MantineProvider, Switch } from '@mantine/core';
 
 export default function App() {
   return (
@@ -8,16 +8,37 @@ export default function App() {
 
 function Demo() {
   return (
-    <MantineProvider
-      theme={{
-        fontFamily: 'Verdana, sans-serif',
-        fontFamilyMonospace: 'Monaco, Courier, monospace',
-        headings: { fontFamily: 'Greycliff CF, sans-serif' },
-      }}
-    >
-      <Title order={3}>Greycliff CF or sans-serif title</Title>
-      <Button>Verdana button</Button>
-      <Code>Monaco, Courier Code</Code>
-    </MantineProvider>
+    <>
+      <MantineProvider
+        theme={{
+          components: {
+            Button: {
+              defaultProps: {
+                size: 'xs',
+                color: 'cyan',
+              },
+            },
+
+            Switch: {
+              defaultProps: {
+                size: 'xs',
+                onLabel: 'ON',
+                offLabel: 'OFF',
+              },
+            },
+          },
+        }}
+      >
+        <Group position="center">
+          <Button>Within provider</Button>
+          <Switch label="Within provider" />
+        </Group>
+      </MantineProvider>
+
+      <Group position="center" mt="md">
+        <Button>Outside of provider</Button>
+        <Switch label="Outside of provider" />
+      </Group>
+    </>
   );
 }
